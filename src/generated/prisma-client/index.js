2553597e38c7a58d76ab69f10eed86fbade2b548
Kirill Gladkov <crosfire2011@gmail.com>
2019-06-25 10:12:05 +0300

    worked server

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Vote",
+    embedded: false
+  },
+  {
+    name: "Link",
+    embedded: false
+  },
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/kirill-gladkov-215994/hackernews-node/dev`
+});
+exports.prisma = new exports.Prisma();
